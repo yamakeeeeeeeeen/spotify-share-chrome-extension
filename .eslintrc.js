4d7dd94102fe8edf616620b3e1d95abd8feff89c
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-02-28 21:50:44 +0900

    fix: change eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,64 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
+  settings: { react: { version: 'detect' } },
+  env: { es2021: true, browser: true, jest: true, node: true },
+  plugins: ['import', 'simple-import-sort', 'react-hooks'],
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier'
   ],
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
-  parser: '@typescript-eslint/parser',
-  env: {
-    browser: true,
-    node: true,
-    es6: true
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
   rules: {
+    'no-restricted-syntax': [
+      'error',
+      { selector: 'TSEnumDeclaration', message: "Don't declare enums" }
+    ],
+    'prefer-arrow-callback': 'error',
+    'func-style': ['error', 'expression'],
+    'arrow-body-style': ['error', 'always'],
+    'no-restricted-imports': [
+      'error',
+      { paths: [{ name: 'react', importNames: ['default'] }] }
+    ],
+    'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
+    'react/display-name': 'error',
+    'react/destructuring-assignment': ['error', 'never'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/newline-after-import': 'error',
+    'import/no-default-export': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      { prefer: 'type-imports' }
+    ],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton']
+      }
+    ]
   },
   overrides: [
     {
-      files: ['*.tsx'],
-      rules: { 'react/prop-types': ['off'] }
-    },
-    {
-      files: ['*.js'],
-      rules: { '@typescript-eslint/no-var-requires': ['off'] }
+      files: ['src/pages/**/*.tsx'],
+      rules: { 'import/no-default-export': 'off' }
     }
   ]
 }
