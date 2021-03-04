8d9268d5bd221b7e3e72071506e7ef256840d664
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-03-04 20:21:34 +0900

    feat: install @chakra-ui/react

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { PopupPage } from 'popup'
 import type { FC } from 'react'
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-import { PopupPage } from '../popup'
-
 export const App: FC = () => {
   return (
     <Router>
-      <PopupPage />
+      <ChakraProvider>
+        <PopupPage />
+      </ChakraProvider>
     </Router>
   )
 }
