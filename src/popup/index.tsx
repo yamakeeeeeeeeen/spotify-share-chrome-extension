36e1073ba8f80348db141ad48fb261f6ddd8e5ec
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-03-06 12:32:19 +0900

    fix: change display size

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@chakra-ui/react'
 import { Player } from 'components/Player'
 import { useAuth } from 'hooks/useAuth'
 import { useSpotify } from 'hooks/useSpotify'
@@ -12,9 +13,9 @@ export const PopupPage: FC = () => {
   const spotify = useSpotify(accessToken)
 
   return (
-    <div style={{ width: 700, height: 700 }}>
+    <Box w={400} h={500} p={4} bgColor="gray.50">
       {!accessToken && <button onClick={login}>login</button>}
       {spotify && <Player spotify={spotify} />}
-    </div>
+    </Box>
   )
 }
