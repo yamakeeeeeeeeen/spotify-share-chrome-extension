b6204174688e4921549d1439aa5f22093cadbfeb
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-03-07 14:35:13 +0900

    feat: create player button style base

---
+++ src/styles.ts
@@ -0,0 +1,14 @@
+export const playerButtonStyles = {
+  bgColor: 'transparent',
+  color: '#b3b3b3',
+  _active: {
+    color: '#b3b3b3'
+  },
+  _hover: {
+    bgColor: 'transparent',
+    color: '#fff'
+  },
+  _focus: {
+    border: 'none'
+  }
+}
