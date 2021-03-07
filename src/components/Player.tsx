dbf5807bce10e0a40fd81d93cc3c05ba667b72a7
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-03-07 14:41:31 +0900

    feat: create some player feature

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,30 +1,81 @@
-import { useCurrentlyPlayingTrack } from 'hooks/useCurrentlyPlayingTrack'
+import { Box, Button, Flex, Image, Text } from '@chakra-ui/react'
+import { ReloadButton } from 'components/ReloadButton'
+import { TwitterShareButton } from 'components/TwitterShareButton'
+import { usePlayer } from 'hooks/usePlayer'
 import type { FC } from 'react'
 import React from 'react'
+import { BiPause, BiPlay, BiSkipNext, BiSkipPrevious } from 'react-icons/bi'
 import type { SpotifyWebApi } from 'spotify-web-api-ts'
+import { playerButtonStyles } from 'styles'
 
 type Props = {
   spotify: SpotifyWebApi
 }
 type ComponentProps = {
-  track: ReturnType<typeof useCurrentlyPlayingTrack>
+  player: ReturnType<typeof usePlayer>
 }
 
-const Component: FC<ComponentProps> = ({ track }) => (
-  <div>
+const iconSize = 30
+
+const Component: FC<ComponentProps> = ({
+  player: { track, isPlaying, device, play, pause, prev, next, getPlaybackInfo }
+}) => (
+  <Flex justifyContent="center" w={270}>
     {track ? (
-      <div>
-        <h2>{track.name}</h2>
-        <img src={track.album.images[0].url} alt={track.name} />
-      </div>
+      <Box>
+        <Flex justifyContent="center" mb={3}>
+          <Image boxSize="250px" src={track.cover} alt={track.name} />
+        </Flex>
+        <Box mb={4}>
+          <Text mb={2} fontSize="lg" textAlign="center" color="white">
+            {track.name}
+          </Text>
+          <Text fontSize="sm" textAlign="center" color="#b3b3b3">
+            {track.artist} - {track.albumName}
+          </Text>
+        </Box>
+
+        <Flex justifyContent="center">
+          <Button onClick={prev} {...playerButtonStyles}>
+            <BiSkipPrevious size={iconSize} />
+          </Button>
+          {isPlaying ? (
+            <Button onClick={pause} {...playerButtonStyles}>
+              <BiPause size={iconSize} />
+            </Button>
+          ) : (
+            <Button onClick={play} {...playerButtonStyles}>
+              <BiPlay size={iconSize} />
+            </Button>
+          )}
+          <Button onClick={next} {...playerButtonStyles}>
+            <BiSkipNext size={iconSize} />
+          </Button>
+        </Flex>
+
+        <Flex justifyContent="center">
+          <TwitterShareButton track={track} />
+          <ReloadButton getPlaybackInfo={getPlaybackInfo} />
+        </Flex>
+
+        {device && (
+          <Text mt={10} fontSize="sm" textAlign="center" color="#b3b3b3">
+            Playing in {device.name}
+          </Text>
+        )}
+      </Box>
     ) : (
-      <p>{`It's not playing.`}</p>
+      <Text fontSize="lg" textAlign="center" color="#b3b3b3">
+        {`It's not playing.`}
+      </Text>
     )}
-  </div>
+  </Flex>
 )
 
 export const Player: FC<Props> = ({ spotify }) => {
-  const track = useCurrentlyPlayingTrack(spotify)
+  const player = usePlayer(spotify)
 
-  return <Component track={track} />
+  console.log('spotify', spotify.player.getPlaybackInfo())
+
+  return <Component player={player} />
 }
