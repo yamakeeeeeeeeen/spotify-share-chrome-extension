1d86da1cce787ad457b9ad95f38f974c17fb2a1f
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-03-15 22:25:28 +0900

    fix: fix auth method

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -3,14 +3,28 @@ import { Player } from 'components/Player'
 import { useAuth } from 'hooks/useAuth'
 import { useSpotify } from 'hooks/useSpotify'
 import type { FC } from 'react'
-import React from 'react'
-import { getLoginPath } from 'utils/spotify/getLoginPath'
-
-const loginPath = getLoginPath()
+import React, { useEffect, useState } from 'react'
+import { login } from 'utils/spotify/login'
 
 export const PopupPage: FC = () => {
-  const { accessToken, login } = useAuth(loginPath)
+  const { accessToken } = useAuth()
   const spotify = useSpotify(accessToken)
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const token = localStorage.getItem('spotifyAccessToken')
+    const updateTime = localStorage.getItem('spotifyAccessTokenUpdateTime')
+    const now = new Date().getTime()
+    // 55分以内に発行されたトークンであれば有効
+    const isValidToken = now - Number(updateTime) < 1000 * 60 * 55
+
+    if (!token || !updateTime || !isValidToken) {
+      setIsLoggedIn(false)
+      return
+    }
+    setIsLoggedIn(true)
+  }, [])
 
   return (
     <Box
@@ -18,8 +32,11 @@ export const PopupPage: FC = () => {
       p={4}
       background="rgb(18,18,18) linear-gradient(0deg, rgba(18,18,18,1) 0%, rgba(57,57,57,1) 100%)"
     >
-      {!accessToken && <Button onClick={login}>login</Button>}
-      {spotify && <Player spotify={spotify} />}
+      {isLoggedIn ? (
+        spotify && <Player spotify={spotify} />
+      ) : (
+        <Button onClick={login}>login</Button>
+      )}
     </Box>
   )
 }
