f0e4968517c07bbe91d0c5895abe1714c1864ff1
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-03-18 18:55:53 +0900

    refactor: login effect

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,54 +1,106 @@
 import { useQuery } from 'hooks/useQuery'
 import { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import { SPOTIFY } from 'spotify-config'
+import { SpotifyWebApi } from 'spotify-web-api-ts'
 import { getAccessToken } from 'utils/spotify/getAccessToken'
 import {
   getAccessTokenFromLocalStorage,
+  getCodeFromLocalStorage,
   setAccessTokenToLocalStorage,
+  setCodeToLocalStorage,
 } from 'utils/spotify/localStorageOperaters'
+
+const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URIS } = SPOTIFY
 
 export const useAuth = (): {
   isLoggedIn: boolean
-  accessToken: string | null
+  spotify: SpotifyWebApi | null
 } => {
   const { push } = useHistory()
   const query = useQuery()
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [state, setState] = useState<string | null>(null)
+  const [code, setCode] = useState<string | null>(null)
   const [accessToken, setAccessToken] = useState<string | null>(null)
 
-  useEffect(() => {
-    const { token, updateTime } = getAccessTokenFromLocalStorage()
-    const now = new Date().getTime()
-    // 55分以内に発行されたトークンであれば有効
-    const isValidToken = now - Number(updateTime) < 1000 * 60 * 55
+  const [spotify, setSpotify] = useState<SpotifyWebApi | null>(null)
 
-    if (!token || !updateTime || !isValidToken) {
-      setIsLoggedIn(false)
-      setAccessToken(null)
-      return
-    }
-    setIsLoggedIn(true)
-    setAccessToken(token)
+  useEffect(() => {
+    setCode(query.get('code'))
+    setState(query.get('state'))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   // NOTE: spotifyのログインを行ないリダイレクトされた時に実行される
   useEffect(() => {
-    const code = query.get('code')
-    const state = query.get('state')
-
     if (code && state) {
+      setCodeToLocalStorage(code)
+
       getAccessToken(code as string)
         .then((token) => {
           setAccessTokenToLocalStorage(token)
           setAccessToken(token)
           setIsLoggedIn(true)
+          setSpotify(createSpotifyInstance(token))
         })
         .catch((reason) => {
           throw new Error(reason)
         })
       push('/')
     }
-  }, [push, query])
+  }, [code, push, state])
 
-  return { isLoggedIn, accessToken }
+  useEffect(() => {
+    const { token, updateTime } = getAccessTokenFromLocalStorage()
+
+    // ■初めて = access tokenがない
+    if (!token) return
+
+    const now = new Date().getTime()
+    // 55分以内に発行されたトークンであれば有効
+    const isValidToken = now - Number(updateTime) < 1000 * 60 * 55
+
+    // ■ログインしたことがあり(localStorageにtokenがある)、1時間以内のアクセス
+    if (isValidToken) {
+      setSpotify(createSpotifyInstance(token))
+      setAccessToken(token)
+      setIsLoggedIn(true)
+      return
+    }
+
+    // ■ログインしたことがあり(localStorageにtokenがある)、tokenの期限が切れている
+    // TODO: useEffectを分割する/codeをlocalStorageのものにする
+    if (!code) return
+    setSpotify(
+      new SpotifyWebApi({
+        clientId: CLIENT_ID || '',
+        clientSecret: CLIENT_SECRET || '',
+        redirectUri: REDIRECT_URIS || '',
+      })
+    )
+    spotify
+      ?.getRefreshedAccessToken(code)
+      .then((value) => {
+        setAccessToken(value.access_token)
+        spotify?.setAccessToken(accessToken as string)
+        setSpotify(spotify)
+        setIsLoggedIn(true)
+      })
+      .catch((reason) => {
+        throw new Error(reason)
+      })
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return { isLoggedIn, spotify }
 }
+
+const createSpotifyInstance = (accessToken: string) =>
+  new SpotifyWebApi({
+    accessToken,
+    clientId: CLIENT_ID || '',
+    clientSecret: CLIENT_SECRET || '',
+    redirectUri: REDIRECT_URIS || '',
+  })
