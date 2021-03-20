1f66e3b3866313bb2a1f23fb6d5f998d2a828010
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-03-20 11:36:07 +0900

    refactor: remove unused methods

--- src/utils/spotify/localStorageOperaters.ts
+++ src/utils/spotify/localStorageOperaters.ts
@@ -1,6 +1,5 @@
 const LOCAL_STORAGE_KEYS = {
   ACCESS_TOKEN: 'spotifyAccessToken',
-  CODE: 'spotifyCode',
   UPDATE_TIME: 'spotifyAccessTokenUpdateTime',
 }
 
@@ -16,10 +15,3 @@ export const getAccessTokenFromLocalStorage = () => ({
   accessToken: localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN),
   updateTime: localStorage.getItem(LOCAL_STORAGE_KEYS.UPDATE_TIME),
 })
-
-export const setCodeToLocalStorage = (code: string) => {
-  localStorage.setItem(LOCAL_STORAGE_KEYS.CODE, code)
-}
-
-export const getCodeFromLocalStorage = () =>
-  localStorage.getItem(LOCAL_STORAGE_KEYS.CODE)
